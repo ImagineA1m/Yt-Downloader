9eda377911c5334d6c3e8353ae0597975459b6c8
Your Name <you@example.com>
2023-08-17 18:35:22 -0400

    Final

--- gui/gui.py
+++ gui/gui.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
-from pytube import YouTube
 from tkinter import filedialog
+from pytube import YouTube
 import webbrowser
 
 root = Tk()
@@ -10,7 +10,7 @@ root.title("PreTube : Download Videos Without Youtube Premium") #window title
 background = "white" #background color
 fg = "black" #foreground
 text_color = "black" #text color
-button_color = "red"
+button_color = "#bdbdbd"
 root.config(bg=background) # window color set to background
 root.iconbitmap("images/Logo.ico")
 download_btn = PhotoImage(file="images/PreTubeLogo.png/")
@@ -30,7 +30,7 @@ def start():
 def error():
     messagebox.showerror("PreTube Error", "Invalid Link (URL)")
 
-    
+
 def main():
     file_path = filedialog.askdirectory()
 
@@ -67,9 +67,9 @@ def main():
 
 Input = Entry(root, width=70, fg=text_color, borderwidth=2, relief="groove", bg="white", font="Arial 18")
 button = Button(root, text="Download", fg=text_color, bg="red", borderwidth=0, command=main, font="YouTube-Sans-Bold 35")
-choiceHigh = Radiobutton(root, text="High Quality (Slower) ", variable=q, value=1, height="1", bg=button_color, fg=fg, font="metropolis-extra-bold 19")
-choiceLow = Radiobutton(root, text="Low Quality (Faster)", variable=q, value=2, height="1", bg=button_color, fg=fg, font="metropolis-extra-bold 19")
-choiceAudio = Radiobutton(root,text="Audio Only (Offline Music)", variable=q, value=3, height="1", bg=button_color, fg=fg, font="metropolis-extra-bold 19")
+choiceHigh = Radiobutton(root, text="High Quality (Slower) ", variable=q, value=1, height="1", bg=button_color, fg=fg, font="TravelSunday 19")
+choiceLow = Radiobutton(root, text="Low Quality (Faster)", variable=q, value=2, height="1", bg=button_color, fg=fg, font="TravelSunday 19")
+choiceAudio = Radiobutton(root,text="Audio Only (Offline Music)", variable=q, value=3, height="1", bg=button_color, fg=fg, font="TravelSunday 19")
 
 Input.pack(pady=5,)
 choiceHigh.pack(pady=10)
